037b6216859b71d21e4a2aface5cb90c70fa24b2
league <league@imac-39.attlocal.net>
2025-02-01 16:27:50 -0800

    i like smiley

--- src/_02_static_smiley_blues/SmileyBlues.java
+++ src/_02_static_smiley_blues/SmileyBlues.java
@@ -1,27 +1,36 @@
 package _02_static_smiley_blues;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class SmileyBlues {
 	void start() {
 		// 1. Make a new JFrame and set it to be visible
-		JFrame Crackers= new JFrame();
-		Crackers.setVisible(true);
+		JFrame luke = new JFrame();
+		luke.setVisible(true);
 		// 2. Set your frame's default close operation to JFrame.EXIT_ON_CLOSE
-
+		luke.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 3. Make a new JPanel
-
+		JPanel piggy = new JPanel();
 		// 4. Add your panel to your frame
-
-		// 8. DO THIS LAST: 
-		// Set the Smiley class's color variable to Smiley.YELLOW (NOT a specific smiley object's!)
-		// Using a specific object for a static variable works, but is not usually recommended.
-		// Run it again; are your Smileys all yellow now? Also try changing them to red or green!
+		luke.add(piggy);
+		// 8. DO THIS LAST:
+		// Set the Smiley class's color variable to Smiley.YELLOW (NOT a specific smiley
+		// object's!)
+		Smiley.color=Smiley.YELLOW;
+		// Using a specific object for a static variable works, but is not usually
+		// recommended.
+		// Run it again; are your Smileys all yellow now? Also try changing them to red
+		// or green!
 
 		// 5. Make three Smiley objects and add them to your panel
-
+		Smiley rodent = new Smiley();
+		Smiley doggo = new Smiley();
+		Smiley cat = new Smiley();
+		piggy.add(rodent);
+		piggy.add(doggo);
+		piggy.add(cat);
 		// 6. Pack your frame
-
+		luke.pack();
 		// 7. RUN THE PROGRAM and make sure you see three blue Smileys!
 	}
 }
