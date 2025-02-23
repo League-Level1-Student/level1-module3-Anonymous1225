4fb186644705ac036bdeb8e5c3828dc7ec47210e
league <league@imac-39.attlocal.net>
2025-02-22 16:28:50 -0800

    i like cats

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,10 +3,11 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
-    int x=270;
-	int y=550;
+    static final int WIDTH = 810;
+    static final int HEIGHT = 600;
+    int x=360;
+	int y=540;
+	Cat sam;
     @Override
     public void settings() {
         size(800,600);
@@ -14,7 +15,7 @@ public class Frogger extends PApplet {
 
     @Override
     public void setup() {
-
+    	sam=new Cat(0,400,90,6);
     }
     public void keyPressed()
     {
@@ -22,32 +23,76 @@ public class Frogger extends PApplet {
             if(keyCode == UP)
             {
                 //Frog Y position goes up
-            	y-=30;
+            	y-=10;
             }
             else if(keyCode == DOWN)
             {
-            	y+=30;
+            	y+=10;
                 //Frog Y position goes down 
             }
             else if(keyCode == RIGHT)
             {
                 //Frog X position goes right
-            	x+=30;
+            	x+=10;
             }
             else if(keyCode == LEFT)
             {
                 //Frog X position goes left
-            	x-=30;
+            	x-=10;
             }
+            inside();
+            System.out.println(x);
+        }
+    }
+    public void inside() {
+        if(x>=755) {
+        	x=755;
+        }
+        if(x<=0) {
+        	x=0;
+        }
+        if(y>=540) {
+        	y=540;
+        }
+        if(y<=0) {
+        	y=0;
         }
     }
     @Override
     public void draw() {
     	background(0,210,255);
-    	fill(0,205,255);
-    	
+    	fill(0,20,255);
+    	rect(x,y,45,60);
+    	keyPressed();
+    	sam.display();
+    	sam.move();
     	
     }
+    
+
+    
+    public class Cat{
+    	int catX;
+    	int catY;
+    	int size;
+    	int speed;
+    	public Cat(int catX,int catY,int size,int speed) {
+    		this.catX=catX;
+    	    this.catY=catY;
+    	    this.size=size;
+    	    this.speed=speed;
+    	}
+    	void display()
+    	  {
+    	    fill(255,255,240);
+    	    rect(catX , catY,  size, 50);
+    	  }
+    	void move() {
+    		catX+=speed;
+    	}
+    }
+    
+    
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
     }
